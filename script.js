717a4601d477194b86693b1aaae6ad702d3d6dae
Dev Sharma <sharma.dev4242@gmail.com>
2021-01-02 16:43:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var port = process.env.PORT || 8080;
 console.log('Todo required => ', Todo);
 console.log('Todo from mongoose => ', mongoose.model('TodoModel'));
 
-mongoose.connect(process.env.MONOG_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 app.use('/', express.static(path.resolve(__dirname, 'assets')));
 
